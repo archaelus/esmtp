8a08db1b0a8031ccf638f056cbc27a8d74fe4aa8
Geoff Cant <nem@lisp.geek.nz>
2008-10-21 00:44:10 +0200

    Adding basic OTP application modules.

---
+++ src/esmtp_app.erl
@@ -0,0 +1,56 @@
+%%%-------------------------------------------------------------------
+%% @copyright Geoff Cant
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc ESMTP application module
+%% @end
+%%%-------------------------------------------------------------------
+-module(esmtp_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+-export([config/1]).
+
+%%====================================================================
+%% Application callbacks
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start(Type, StartArgs) -> {ok, Pid} |
+%%                                     {ok, Pid, State} |
+%%                                     {error, Reason}
+%% Description: This function is called whenever an application 
+%% is started using application:start/1,2, and should start the processes
+%% of the application. If the application is structured according to the
+%% OTP design principles as a supervision tree, this means starting the
+%% top supervisor of the tree.
+%%--------------------------------------------------------------------
+start(_Type, StartArgs) ->
+    esmtp_sup:start_link().
+
+%%--------------------------------------------------------------------
+%% Function: stop(State) -> void()
+%% Description: This function is called whenever an application
+%% has stopped. It is intended to be the opposite of Module:start/2 and
+%% should do any necessary cleaning up. The return value is ignored. 
+%%--------------------------------------------------------------------
+stop(_State) ->
+    ok.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+%% @spec config(Item::atom()) -> term()
+%% @doc Retrieve the configuration value for key Item from the tbld
+%% OTP application environment.
+config(Item) ->
+    case application:get_env(tbld, Item) of
+        {ok, Term} -> Term;
+        undefined ->
+            error_logger:error_msg("tbld not correctly configured: missing ~p",
+                                   [Item]),
+            exit(tbld_misconfigured)
+    end.
---
+++ src/esmtp_sup.erl
@@ -0,0 +1,54 @@
+%%%-------------------------------------------------------------------
+%% @copyright Geoff Cant
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc Mail client supervisor
+%% @end
+%%%-------------------------------------------------------------------
+-module(esmtp_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% @spec start_link() -> {ok,Pid} | ignore | {error,Error}
+%% @doc: Starts the supervisor
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Func: init
+%% @spec (Args) -> {ok,  {SupFlags,  [ChildSpec]}} |
+%%                 ignore                          |
+%%                 {error, Reason}
+%% @doc Whenever a supervisor is started using 
+%% supervisor:start_link/[2,3], this function is called by the new process 
+%% to find out about restart strategy, maximum restart frequency and child 
+%% specifications.
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    Clients = {"ESMTP Client",
+               {esmtp_client,start_link,[]},
+               temporary,2000,worker,
+               [esmtp_fsm]},
+    {ok,{{simple_one_for_one,0,1}, [Clients]}}.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
